91b477ce02efb23487e47311366105e182dc5152
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2024-05-04 20:05:53 +0100

    Imported Gherkin keywords from cucumber package.

---
+++ features/step_definitions/step.js
@@ -0,0 +1,17 @@
+const {Given, When, Then} = require('@cucumber/cucumber');
+
+Given('Lucy is {int} metres from Sean', function (int) {
+    // Given('Lucy is {float} metres from Sean', function (float) {
+      // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+  
+When('Sean shouts {string}', function (string) {
+      // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
+  
+Then('Lucy hears Sean\'s message', function () {
+      // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+});
